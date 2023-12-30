eb1b180a1d8156324c95e733e48d4a4da0e5e04e
Shahhoseini.Maziar <Shahhoseini.Maziar@SGS001P10361>
2023-12-30 17:51:26 +0330

    Add project files.

---
+++ MementoDesignPattern/Basket.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MementoDesignPattern
+{
+    public class BasketSnapShot
+    {
+        public Basket Basket { get; set; }
+    }
+    public class Basket
+    {
+        private int id;
+        private DateTime CreateDate;
+        private List<Product> products = new List<Product>();
+        public void Add(Product product)
+        {
+            products.Add(product);
+        }
+        public void Remove(Product product)
+        {
+            products.Remove(product);
+        }
+        public decimal GetTotalPrice()
+        {
+            return products.Sum(x =>x.Price);
+        }
+        public Basket CreateSnapShot()
+        {
+            return new Basket
+            {
+                id = id,
+                products = new List<Product>(products)
+            };
+        }
+
+
+        public void SetSnapShot(Basket basket)
+        {
+            Console.WriteLine("Restoring state...");
+            this.products = basket.products;
+        }
+
+
+    }
+    public class Product
+    {
+        public Product(int id, int price)
+        {
+            Id = id;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ MementoDesignPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MementoDesignPattern;
+
+Console.WriteLine("Hello, World!");
+
+Product product = new Product(1,10);
+Product product1 = new Product(1, 10);
+
+Basket basket = new Basket();
+
+basket.Add(product1);
+basket.Add(product);
+
+
+BasketSnapShot basketSnapShot = new BasketSnapShot();
+basketSnapShot.Basket = basket.CreateSnapShot();
+
+Console.WriteLine(basket.GetTotalPrice());
+
+basket.Remove(product);
+
+Console.WriteLine(basket.GetTotalPrice());
+
+
+basket.SetSnapShot(basketSnapShot.Basket);
+
+Console.WriteLine(basket.GetTotalPrice());
+
